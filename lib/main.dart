1a1fb96caa96015d483f886716f6444696d1ef1a
Ali Elahi <alielahi.1378@gmail.com>
2021-08-28 18:55:42 +0430

    named routes are added to the project and logo image is added to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flash_messenger/screens/chat_screen.dart';
+import 'package:flash_messenger/screens/login_screen.dart';
+import 'package:flash_messenger/screens/registration_screen.dart';
 import 'package:flash_messenger/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +10,19 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: '/welcome_screen',
+      routes: {
+        '/welcome_screen': (context) => WelcomeScreen(),
+        '/login_screen': (context) => LoginScreen(),
+        '/registration_screen': (context) => RegistrationScreen(),
+        '/chat_screen': (context) => ChatScreen(),
+      },
     );
   }
 }
